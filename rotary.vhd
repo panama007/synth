c1b04a07aa9303ab2cb106ed331f4af67d339e1e
Alex Barreiro <panama007@gmail.com>
2016-01-31 02:20:12 -0800

    changes

--- rotary.vhd
+++ rotary.vhd
@@ -1,74 +1,94 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.all;
 
 entity rotary is
     generic (bits: integer := 16);
     port    (AB  : in std_logic_vector(1 downto 0);
+             clk : in std_logic;
+             test: out std_logic_vector(2 downto 0);
              freq: out std_logic_vector(bits-1 downto 0));
 end rotary;
 
-architecture Behavioral of LCD_driver is
+architecture Behavioral of rotary is
     signal freq_int : std_logic_vector(bits-1 downto 0) := (bits-1 downto 1 => '0') & '1';
-    signal state    : std_logic_vector(1 downto 0) := "000";
+    signal current_state    : std_logic_vector(2 downto 0) := "000";
+    signal next_state    : std_logic_vector(2 downto 0) := "000";
 begin
-
-process (clk) 
-begin
-    if rising_edge(clk) then
-        case state is
-            when "000" => 
-                case AB is
-                    when "01" => state <= "001";
-                    when "10" => state <= "111";
-                    when others => state <= "000"; 
-            when "001" => 
-                case AB is
-                    when "11" => state <= "010";
-                    when "00" => state <= "000";
-                    when others => state <= "001"; 
-            when "010" => 
-                case AB is
-                    when "10" => state <= "011";
-                    when "01" => state <= "001";
-                    when others => state <= "010";
-            when "011" => 
-                case AB is
-                    when "00" => 
-                        state <= "000";
-                        if not freq_int(bits-1) then 
-                            freq_int <= freq_int sll 1;
-                        end if;
-                    when "11" => state <= "010";
-                    when others => state <= "011";
-                    
-                     
-            when "101" => 
-                case AB is
-                    when "00" => 
-                        state <= "000";
-                        if not freq_int(0) then 
-                            freq_int <= freq_int srl 1;
-                        end if;
-                    when "11" => state <= "110";
-                    when others => state <= "101"; 
-            when "110" => 
-                case AB is
-                    when "01" => state <= "101";
-                    when "10" => state <= "111";
-                    when others => state <= "110"; 
-            when "111" => 
-                case AB is
-                    when "11" => state <= "110";
-                    when "00" => state <= "000";
-                    when others => state <= "111"; 
-            
-            when others => state <= "000";
-        end case;
-    end if;
-    
+    test <= current_state;
     freq <= freq_int;
+
+
+process (AB) 
+begin
+    case current_state is
+        when "000" => 
+            case AB is
+                when "01" => next_state <= "001";
+                when "10" => next_state <= "111";
+                when others => next_state <= "000"; 
+            end case;
+        when "001" => 
+            case AB is
+                when "11" => next_state <= "010";
+                when "00" => next_state <= "000";
+                when others => next_state <= "001"; 
+            end case;
+        when "010" => 
+            case AB is
+                when "10" => next_state <= "011";
+                when "01" => next_state <= "001";
+                when others => next_state <= "010"; 
+            end case;
+        when "011" => 
+            case AB is
+                when "00" => 
+                    next_state <= "000";
+                    freq_int <= (bits-1 downto 0 => '0');
+                    --if freq_int(bits-1) = '0' then 
+                        --freq_int <= freq_int(bits-2 downto 0) & '0';
+                        
+                    --end if;
+                when "11" => next_state <= "010";
+                when others => next_state <= "011"; 
+            end case;
+                
+                 
+        when "101" => 
+            case AB is
+                when "00" =>
+                    next_state <= "000";
+                    freq_int <= (bits-1 downto 0 => '1');
+                    --if freq_int(0) = '0' then 
+                        --freq_int <= '0' & freq_int(bits-1 downto 1);
+                        
+                    --end if;
+                when "11" => next_state <= "110";
+                when others => next_state <= "101"; 
+            end case; 
+        when "110" => 
+            case AB is
+                when "01" => next_state <= "101";
+                when "10" => next_state <= "111";
+                when others => next_state <= "110"; 
+            end case; 
+        when "111" => 
+            case AB is
+                when "11" => next_state <= "110";
+                when "00" => next_state <= "000";
+                when others => next_state <= "111"; 
+            end case; 
+        
+        when others => next_state <= "000";
+    end case;
 end process;
 
+process (clk)
+begin
+    if rising_edge(clk) then
+        current_state <= next_state;
+    end if;
+end process;
 end Behavioral;
 
--- test_top.vhd
+++ test_top.vhd
@@ -10,6 +10,7 @@ entity test_top is
            latch   : in std_logic;
            switches: in std_logic_vector(7 downto 0);
            keyboard: in std_logic_vector(11 downto 0);
+           rotary1 : in std_logic_vector(1 downto 0);
            cathodes: out std_logic_vector(6 downto 0);
            anodes  : inout std_logic_vector(3 downto 0));
 end test_top;
@@ -17,15 +18,23 @@ end test_top;
 architecture Behavioral of test_top is
     signal freq : std_logic_vector(bits-1 downto 0);
     signal waveform : std_logic_vector(bits-1 downto 0);
+    signal to_disp : std_logic_vector(bits-1 downto 0);
+    signal test : std_logic_vector(2 downto 0);
 begin
-    freq <= keyboard(11) & keyboard(9) & (bits-3 downto 0 => '0');
+    --freq <= keyboard(11) & keyboard(9) & (bits-3 downto 0 => '0');
+    to_disp <= test & (bits-4 downto 0 => '0') when switches(0) = '1' else
+               freq;
     
     LCD : entity work.LCD_driver 
-        generic map (bits => 16, clk_div => 10)
-        port map (latch => clk, clk => clk, freq => freq, cathodes => cathodes, anodes => anodes);
+        generic map (bits => bits, clk_div => 10)
+        port map (latch => clk, clk => clk, freq => to_disp, cathodes => cathodes, anodes => anodes);
     
     OSC : entity work.osc
-        generic map (bits => 16, n => 20)    
+        generic map (bits => bits, n => 20)    
         port map (freq => freq, wave => switches(1 downto 0), clk => clk, output => waveform);
+        
+    ROT : entity work.rotary
+        generic map (bits => bits)
+        port map (AB => rotary1, freq => freq, clk => clk, test => test);
 
 end Behavioral;
